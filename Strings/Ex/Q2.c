58854199e76a930d529d5ef015be6bcf31eb00e0
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 16:54:45 +0530

    Update Q2.c

--- Strings/Ex/Q2.c
+++ Strings/Ex/Q2.c
@@ -1,1 +1,14 @@
 
+// Write a C Program to Find the Length of a String
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100];
+    printf("\nEnter a String: ");
+    scanf("%s", str);
+    int length = strlen(str);
+    printf("\nLength of String is %d\n", length);
+    return 0;
+}
