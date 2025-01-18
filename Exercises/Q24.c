a50c1970ec17765d4df2eb3cd00d167147dd7389
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 15:15:42 +0530

    Update Q24.c

--- Exercises/Q24.c
+++ Exercises/Q24.c
@@ -1,1 +1,17 @@
 
+// Write a C program random number generator
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("The random number %d is %d\n", i + 1, rand() % 100);
+    }
+    return 0;
+}
