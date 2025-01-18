e4122516b07359b9f3fa643403d7e9d38cace893
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 15:10:21 +0530

    Update Q16.c

--- Exercises/Q16.c
+++ Exercises/Q16.c
@@ -1,1 +1,17 @@
 
+// C Program to Print Simple Reverse Pyramid Pattern 
+
+#include<stdio.h>
+int main()
+{
+    int i, j;
+    for(i=0; i<=4; i++)
+    {
+        for(j=4; j >= i; j--)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
