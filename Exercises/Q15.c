f90c07f975549279853b448e916313f4ccc54f66
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 15:09:50 +0530

    Update Q15.c

--- Exercises/Q15.c
+++ Exercises/Q15.c
@@ -1,1 +1,17 @@
 
+// C Program to Print Simple Pyramid Pattern 
+
+#include<stdio.h>
+int main()
+{
+    int i, j;
+    for(i = 0; i <= 5; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}s
