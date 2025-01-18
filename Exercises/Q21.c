8b2484dc79a14004d51983477830ec971715e362
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 15:13:57 +0530

    Update Q21.c

--- Exercises/Q21.c
+++ Exercises/Q21.c
@@ -1,1 +1,12 @@
 
+// Area of Square
+#include <stdio.h>
+int main()
+{
+    float side, area;
+    printf("Enter the side of square: ");
+    scanf("%f", &side);
+    area = side * side;
+    printf("Area of square: %f", area);
+    return 0;
+}s
