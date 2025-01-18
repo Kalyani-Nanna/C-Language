dbe46aa483ee050c24e56df980e46d9360476adc
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 17:17:20 +0530

    Update IncDecOpr.c

--- Operators/Increment-Decrement-Operator/IncDecOpr.c
+++ Operators/Increment-Decrement-Operator/IncDecOpr.c
@@ -1,1 +1,18 @@
 
+//Increment & Decrement Operator
+
+#include<stdio.h>
+int main()
+{
+    int a = 11, b = 8;
+    a++;
+    b--;
+    printf("a = %d, b = %d", a, b);
+    return 0;
+}
+
+// They are used to increment / decrement a variable value by 1.
+/*
+output
+a = 12, b = 7
+*/
