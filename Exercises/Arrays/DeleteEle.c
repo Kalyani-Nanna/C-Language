ddba8d0928bbdcb985e133dc080ad9a6bb15cc3c
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 15:22:38 +0530

    Update DeleteEle.c

--- Exercises/Arrays/DeleteEle.c
+++ Exercises/Arrays/DeleteEle.c
@@ -1,1 +1,77 @@
 
+/*
+Program to delete an element from an array using
+skipping method.
+*/
+
+#include<stdio.h>
+void main()
+{
+    int a[100], n, i, j, ele, f=0;
+    printf("\nEnter array size: ");
+    scanf("%d", &n);
+    printf("\nEnter %d elements: ", n);
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("\nEnter element to delete: ");
+    scanf("%d", &ele);
+
+   
+    printf("\nArray is: ");
+    for(i=0; i<n; i++)
+    {
+        printf("%2d", a[i]);
+    }
+
+    printf("\nAfter Deleting element array is: ");
+    for(i=0; i<n; i++)
+    {
+        if(a[i] != ele)
+        {
+            printf("%2d", a[i]);
+        }else{
+            f=1;
+        }
+    }
+    if(f==0)
+    {
+        printf("\nElment not found");
+    }
+
+}
+
+/*
+Enter array size: 5
+
+Enter 5 elements: 1
+2
+3
+4
+5
+
+Enter element to delete: 3
+
+Array is:  1 2 3 4 5
+After Deleting element array is:  1 2 4 5
+*/
+
+/*
+Enter array size: 5
+
+Enter 5 elements: 1
+2
+3
+4
+5
+
+Enter element to delete: 6
+
+Array is:  1 2 3 4 5
+
+After Deleting element array is:  1 2 3 4 5
+
+Elment not found
+*/
