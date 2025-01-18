9c4a5eef618728247a6a7de9eb8fc33b021c9195
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 15:02:03 +0530

    Update Q13.c

--- Exercises/Q13.c
+++ Exercises/Q13.c
@@ -1,1 +1,15 @@
 
+// Write a C Program to Generate Multiplication Table 
+
+#include<stdio.h>
+int main()
+{
+    int num, i = 1;
+    printf("\nEnter a Number to generate Table: ");
+    scanf("%d",&num);
+    for(i = 1; i <= 10; i++)
+    {
+        printf("%d ", num * i);
+    }
+    return 0;
+}
