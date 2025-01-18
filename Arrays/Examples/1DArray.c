aea7e98d374ef3c2317a49223382b5368c1c7974
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 14:02:29 +0530

    Update 1DArray.c

--- Arrays/Examples/1DArray.c
+++ Arrays/Examples/1DArray.c
@@ -1,1 +1,13 @@
 
+// write a program to print 1D array
+
+#include <stdio.h>
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
