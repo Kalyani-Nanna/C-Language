8ae1645eb72cd21c524a5edff7d9826fc33f5c90
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 15:02:47 +0530

    Update Q14.c

--- Exercises/Q14.c
+++ Exercises/Q14.c
@@ -1,1 +1,17 @@
 
+// C Program to Print Simple Square Pattern 
+
+#include<stdio.h>
+int main()
+{
+    int i, j;
+    for(i=0; i<5; i++)
+    {
+        for(j=0; j<5; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
