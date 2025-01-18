3e867b07f333a45ead48a4c013425aa2fe564aeb
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 16:53:07 +0530

    Update Uppercase.c

--- Strings/Basics/Uppercase.c
+++ Strings/Basics/Uppercase.c
@@ -1,1 +1,29 @@
 
+/*
+Write a C program to convert a string to uppercase
+*/
+
+#include<stdio.h>
+void main()
+{
+    char s[100];
+    int i, j;
+
+    printf("\nEnter a String: ");
+    scanf("%[^\n]", s);
+
+    printf("\nUpper case string : ");
+    for(i=0; s[i]!='\0'; i++)
+    {
+        if(s[i] >= 'a' && s[i] <= 'z'){
+            s[i]-=32;
+        }
+        printf("%c", s[i]);
+    }
+}
+
+/*
+Enter a String: hello how are you bro?
+
+Upper case string : HELLO HOW ARE YOU BRO?
+*/
