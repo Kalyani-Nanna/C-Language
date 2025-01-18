8438b175b83d1fe185a1ce6104b048bb1efa3283
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 14:16:35 +0530

    Update Square.c

--- Basics/Area/Square.c
+++ Basics/Area/Square.c
@@ -1,1 +1,13 @@
 
+//Find Area of sqauare
+
+#include<stdio.h>
+int main()
+{
+    float area, side;
+    printf("\nEnter side of square: ");
+    scanf("%f",&side);
+    area = (side * side);
+    printf("Area of Square = %f", area);
+    return 0;
+}
