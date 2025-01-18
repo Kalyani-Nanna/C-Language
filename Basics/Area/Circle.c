06588d21373c7986c2424990b909794dd98a1e96
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 14:14:52 +0530

    Update Circle.c

--- Basics/Area/Circle.c
+++ Basics/Area/Circle.c
@@ -1,1 +1,13 @@
 
+//Find Area of circle
+
+#include<stdio.h>
+int main()
+{
+    float area, radius, pi = 3.14;
+    printf("\nEnter Radius of Circle: ");
+    scanf("%f", &radius);
+    area = pi * radius * radius;
+    printf("\nArea of Circle = %f", area);
+    return 0;
+}
