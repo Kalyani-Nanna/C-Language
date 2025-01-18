6a6f3d57cad6321ecc69a0cf60531c9c5d9b5c73
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 15:19:24 +0530

    Update Q8.c

--- Exercises/Q8.c
+++ Exercises/Q8.c
@@ -1,1 +1,13 @@
 
+// Write a C Program to Print an Integer Entered By the User
+
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter an Integer: ");
+    scanf("%d", &n);
+    printf("Entered Integer by User is %d", n);
+    return 0;
+}
