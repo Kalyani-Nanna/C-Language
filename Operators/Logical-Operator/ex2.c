be058bddab2eacd0fbef554bb176ad1d2bbf74bb
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 17:24:59 +0530

    Update ex2.c

--- Operators/Logical-Operator/ex2.c
+++ Operators/Logical-Operator/ex2.c
@@ -1,1 +1,16 @@
 
+//Logical Operators Example
+
+#include<stdio.h>
+int main()
+{
+    printf("") && printf("Hi ");
+    printf("Hello ") || printf("Thank you ");
+    printf("See you ") && printf("Have a nice day");
+    return 0;
+}
+
+/*
+output
+Hello See you Have a nice day
+*/
