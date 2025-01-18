233f9758c62d47037d4b62536470347a42118916
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 16:08:52 +0530

    Update ex1.c

--- Loops-Iterations/While-Loop/ex1.c
+++ Loops-Iterations/While-Loop/ex1.c
@@ -1,1 +1,24 @@
 
+//While Loop example
+
+#include<stdio.h>
+void main()
+{
+    int n, i=1;
+    printf("\nEnter a number: ");
+    scanf("%d", &n);
+    while(i<=n)
+    {
+        printf("\n%d", i++);
+    }
+}
+
+/*
+Enter a number: 5
+
+1
+2
+3
+4
+5
+*/
