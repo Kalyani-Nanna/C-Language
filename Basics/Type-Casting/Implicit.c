0f355b13c3f57521661731b7d9113f47c69bedc2
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 14:33:40 +0530

    Update Implicit.c

--- Basics/Type-Casting/Implicit.c
+++ Basics/Type-Casting/Implicit.c
@@ -1,1 +1,10 @@
 
+#include<stdio.h>
+int main()
+{
+    int a = 2.5;    //Implicit Type Casting
+    float b = 5;    //Implicit Type Casting
+    printf("a = %d", a);
+    printf("\nb = %f", b);
+    return 0;
+}
