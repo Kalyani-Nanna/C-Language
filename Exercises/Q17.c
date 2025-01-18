b85ea718bb4e6dcd9aec3a704496f820bf8c1396
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 15:11:02 +0530

    Update Q17.c

--- Exercises/Q17.c
+++ Exercises/Q17.c
@@ -1,1 +1,26 @@
 
+//Write a C Program to Print the numbers pyramid
+
+#include<stdio.h>
+
+int main()
+{
+    int i, j;
+    for(i = 1; i <= 5; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+
+    for(i = 1; i <= 5; i++)
+    {
+        for(j = 5; j >= i; j--)
+        {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+}
