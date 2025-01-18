cfa694df17640cf167e449434ebd43ad635302e1
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 15:16:28 +0530

    Update Q3.c

--- Exercises/Q3.c
+++ Exercises/Q3.c
@@ -1,1 +1,13 @@
 
+//Write a C Program to Find the Size of int, float, double, and char
+
+#include<stdio.h>
+
+int main()
+{
+    printf("\nSize of int is %lu bytes", sizeof(int));
+    printf("\nSize of float is %lu bytes", sizeof(float));
+    printf("\nSize of double is %lu bytes", sizeof(double));
+    printf("\nSize of char is %lu bytes", sizeof(char));
+    return 0;
+}
