f5937ceb2875498d191ae40c7677e26bbcb44137
mothkur kalyani <mothkurkalyani3@gmail.com>
2025-01-18 16:59:08 +0530

    Update pg2.c

--- Programs/pg2.c
+++ Programs/pg2.c
@@ -1,1 +1,18 @@
 
+/*
+Write a C program to take principle amount, time, rateof interest 
+and Find simple interest on principle amount
+*/
+
+#include<stdio.h>
+void main()
+{
+    float p, si, total, ri;
+    int time;
+    printf("\nEnter amount, time, rate: ");
+    scanf("%f%d%f", &p, &time, &ri);
+    si = (p * time / 100) * ri;
+    total = p + si;
+    printf("\nSimple interest is %.2f", si);
+    printf("\nTotal amount is %.2f", total);
+}
